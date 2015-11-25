e92937e30b00c89efc11ddff335c51f363d721c3
jhavlena <jhavlena@moravia.com>
2015-11-25 21:13:13 +0100

    test commit
    test commit

---
+++ WebApplication1testgithub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication1testgithub.Startup))]
+namespace WebApplication1testgithub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
